f92dc4a640d65f7db65c393fd3537b178aa5cddb
Augusto Omena <augusto.omena.n@gmail.com>
2023-10-20 20:04:09 -0300

    Adicionando classe Sturtup e criando branch secundaria

---
+++ MeuSiteEmMVC/Startup.cs
@@ -0,0 +1,20 @@
+﻿namespace MeuSiteEmMVC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment environment) 
+        { 
+        
+        }
+    }
+}
