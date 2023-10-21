ce65f61979cbc04b2a864297a9299bb8d72e9222
Augusto Omena <augusto.omena.n@gmail.com>
2023-10-20 21:56:00 -0300

    Preparando classe Startup e fazendo adaptações.

--- MeuSiteEmMVC/Startup.cs
+++ MeuSiteEmMVC/Startup.cs
@@ -1,4 +1,14 @@
-﻿namespace MeuSiteEmMVC
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuSiteEmMVC
 {
     public class Startup
     {
@@ -10,11 +20,32 @@
 
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddControllersWithViews();
         }
-        public void Configure(WebApplication app, IWebHostEnvironment environment) 
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
         { 
-        
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            } else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id}");
+
+            });
         }
     }
 }
